5e31970dcd560c8e996d84ba35e3d3498ab27ebc
isalzufari <isalzufari@gmail.com>
2023-03-13 10:00:06 +0700

    threads states

--- src/states/index.js
+++ src/states/index.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 
 // Reducer
 import authUserReducer from './authUser/reducer';
+import threadsReducer from './threads/reducer';
 import usersReducer from './users/reducer';
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
     authUser: authUserReducer,
+    threads: threadsReducer,
   }
 });
 
